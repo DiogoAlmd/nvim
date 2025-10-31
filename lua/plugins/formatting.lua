93224fff7bde283dc0227666fbc47deb434946be
diogoalmd <diogo.meida@gmail.com>
2025-10-31 13:53:54 -0300

    fix: linting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,64 +1,67 @@
-return {
-  {
+return {{
     "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
+    event = {"BufWritePre"},
+    cmd = {"ConformInfo"},
+    keys = {{
         "<leader>fm",
         function()
-          require("conform").format({ async = true, lsp_fallback = true })
+            require("conform").format({
+                async = true,
+                lsp_fallback = true
+            })
         end,
-        desc = "Format buffer",
-      },
-    },
+        desc = "Format buffer"
+    }},
     opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        javascript = { "biome", "prettierd", "prettier" },
-        typescript = { "biome", "prettierd", "prettier" },
-        javascriptreact = { "biome", "prettierd", "prettier" },
-        typescriptreact = { "biome", "prettierd", "prettier" },
-        json = { "biome", "prettierd", "prettier" },
-        yaml = { "prettierd", "prettier" },
-        markdown = { "prettierd", "prettier" },
-        html = { "prettierd", "prettier" },
-        css = { "prettierd", "prettier" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      -- Correção da opção obsoleta:
-      -- stop_on_first_success substitui o uso de {{}}
-      formatters = {
-        biome = {
-          command = "biome",
-          args = { "format", "--stdin-file-path", "$FILENAME" },
-          stdin = true,
+        formatters_by_ft = {
+            lua = {"stylua"},
+            python = {"isort", "black"},
+            javascript = {"biome", "prettierd", "prettier"},
+            typescript = {"biome", "prettierd", "prettier"},
+            javascriptreact = {"biome", "prettierd", "prettier"},
+            typescriptreact = {"biome", "prettierd", "prettier"},
+            json = {"biome", "prettierd", "prettier"},
+            yaml = {"prettierd", "prettier"},
+            markdown = {"prettierd", "prettier"},
+            html = {"prettierd", "prettier"},
+            css = {"prettierd", "prettier"}
         },
-      },
-    },
-  },
-  {
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true
+        }
+    }
+}, {
     "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
+    event = {"BufReadPre", "BufNewFile"},
     config = function()
-      local lint = require("lint")
+        local lint = require("lint")
 
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        lint.linters_by_ft = {
+            javascript = {"biomejs"},
+            typescript = {"biomejs"},
+            javascriptreact = {"biomejs"},
+            typescriptreact = {"biomejs"},
+            json = {"biomejs"}
+            -- python = { "pylint" },
+            -- lua = { "luacheck" },
+        }
 
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
+        local lint_augroup = vim.api.nvim_create_augroup("lint", {
+            clear = true
+        })
 
-      vim.keymap.set("n", "<leader>l", function()
-        lint.try_lint()
-      end, { desc = "Trigger linting for current file" })
-    end,
-  },
-}
+        vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost", "InsertLeave"}, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end
+        })
+
+        vim.keymap.set("n", "<leader>l", function()
+            lint.try_lint()
+        end, {
+            desc = "Trigger linting for current file"
+        })
+    end
+}}

4753b156f8a882657ef44ededabb476bfc813f04
diogoalmd <diogo.meida@gmail.com>
2025-10-31 13:58:41 -0300

    feat: filepath

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -42,7 +42,10 @@ return {{
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {{
+                    'filename',
+                    path = 1
+                }},
                 lualine_x = {'encoding', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
